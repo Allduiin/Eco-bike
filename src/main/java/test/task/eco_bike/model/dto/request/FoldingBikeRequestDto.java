e9ff2ff8157a764566e09fe9f5f75e055efb6668
Volodymyr Protsenko <vovapro01@gmail.com>
2020-08-28 16:13:08 +0300

    Added ReadFromFileServiceImpl but not checked does it works

--- src/main/java/test/task/eco_bike/model/dto/request/FoldingBikeRequestDto.java
+++ src/main/java/test/task/eco_bike/model/dto/request/FoldingBikeRequestDto.java
@@ -1,11 +1,15 @@
 package test.task.eco_bike.model.dto.request;
 
 import com.sun.istack.NotNull;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import test.task.eco_bike.model.dto.request.abstraction.BikeRequestDto;
 
+@EqualsAndHashCode(callSuper = true)
+@Data
 public class FoldingBikeRequestDto extends BikeRequestDto {
     @NotNull
     private Long sizeOfTheWheels;
     @NotNull
-    private Long numberOfWheels;
+    private Long numberOfGears;
 }
