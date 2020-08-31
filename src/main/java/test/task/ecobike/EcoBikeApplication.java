1c30635854688eeb47c84f8111aacff8829e771a
Volodymyr Protsenko <vovapro01@gmail.com>
2020-08-31 14:28:17 +0300

    removed unnecessary things at EcoBikeApplication

--- src/main/java/test/task/ecobike/EcoBikeApplication.java
+++ src/main/java/test/task/ecobike/EcoBikeApplication.java
@@ -3,16 +3,10 @@ package test.task.ecobike;
 import java.io.IOException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import test.task.ecobike.service.FoldingBikeService;
-import test.task.ecobike.service.ReadFromFileService;
 
 @SpringBootApplication
 public class EcoBikeApplication {
-    public static void main(String[] args) throws IOException {
-        ConfigurableApplicationContext run = SpringApplication.run(EcoBikeApplication.class, args);
-        run.getBean(ReadFromFileService.class)
-                .read("D:\\Java\\ecobike\\src\\main\\resources\\TestFile.txt");
-        System.out.println(run.getBean(FoldingBikeService.class).getAll());
+    public static void main(String[] args) {
+        SpringApplication.run(EcoBikeApplication.class, args);
     }
 }
